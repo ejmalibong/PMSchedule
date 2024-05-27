6374a7cf9a2ecb6bbcdae1fd026540f187904a25
EJ Malibong <ej.malibong15@gmail.com>
2024-05-27 21:43:22 +0800

    Update connection to development workstation
    CHANGE
    - Reconfigure the connection string to access the development workstation
    
    PENDING
    - Create process management of preventive maintenance schedule
    - Create an auto-generate function of monthly, quarterly, and yearly reports of preventive maintenance

--- Class/Connection.vb
+++ Class/Connection.vb
@@ -5,7 +5,7 @@
             If Environment.MachineName.ToString.Trim = "NBCP-DT-032" Then
                 Return "Data Source=NBCP-DT-032\SQLEXPRESS;Initial Catalog=MachineMonitoring;Persist Security Info=False;User ID=sa;Password=Nbc12#"
             Else
-                Return "Data Source=NBCP-LT-043\SQLEXPRESS;Initial Catalog=MachineMonitoring;Persist Security Info=False;User ID=sa;Password=Nbc12#"
+                Return "Data Source=NBCP-LT-144\SQLEXPRESS;Initial Catalog=MachineMonitoring;Persist Security Info=False;User ID=sa;Password=Nbc12#"
             End If
         Else
             Return "Data Source=LENOVO-AX3RONG2;Initial Catalog=MachineMonitoring;Persist Security Info=False;User ID=sa;Password=Nbc12#"
